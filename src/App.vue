e45a6f6fb8d53eb5f0768c498b6deaa12f614519
Zai <bluekine727@gmail.com>
2025-09-06 21:35:02 +0800

    creat

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <!-- <router-link to="/">首頁</router-link> -->
+      </nav>
+    </header>
+
+    <router-view></router-view>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import './style.css'
+import App from './App.vue'
+
+const routes = [
+
+]
+const router = createRouter({
+    history:createWebHistory(),
+    routes,
+})
+
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
